e66fd886fbc6d8acc9fc1c3dc4b7e994f2db1707
Jay French <jay@frenchfryz.com>
2014-09-29 08:49:26 -0500

    New Domain project

---
+++ Domain/Team/Team.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Team
+{
+    public class Team
+    {
+        public int TeamId { get; private set; }
+        public int Year { get; private set; }
+        public string Name { get; private set; }
+        public string Division { get; private set; }
+        public int ChurchId { get; private set; }
+        public string ChurchName { get; private set; }
+        public int HeadCoachId { get; private set; }
+        public string HeadCoachName { get; private set; }
+        public string FieldInformation { get; private set; }
+        public string Comments { get; private set; }
+    }
+}
