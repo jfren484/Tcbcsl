790165bd1bfbaf4bc4e3c590be61045f6e277738
jfren484 <jay@frenchfryz.com>
2015-06-21 23:31:58 -0500

    Added Standings controller

---
+++ Presentation/Controllers/StandingsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Tcbcsl.Presentation.Models;
+
+namespace Tcbcsl.Presentation.Controllers
+{
+    public class StandingsController : ControllerBase
+    {
+        [Route("Standings/{type}/{year:year?}")]
+        public ActionResult Standings(string type, int year = Config.CurrentYear)
+        {
+            var divisions = DbContext
+                .DivisionYears
+                .Where(dy => dy.Year == year && dy.IsInLeague)
+                .OrderBy(dy => dy.ConferenceYear.Sort)
+                .ThenBy(dy => dy.Sort)
+                .Select(dy => new DivisionStandingsModel
+                {
+                    DivisionName = dy.Name,
+                    Teams = dy.TeamYears
+                        .Select(ty => new StandingsTeamModel
+                        {
+                            TeamId = ty.TeamId,
+                            TeamName = string.IsNullOrEmpty(ty.TeamName)
+                                ? ty.Church.DisplayName
+                                : ty.Church.DisplayName + " " + ty.TeamName
+                        })
+                        .ToList()
+                })
+                .ToList();
+
+            return View(new StandingsModel
+            {
+                Year = year,
+                Divisions = divisions
+            });
+        }
+    }
+}
---
+++ Presentation/Models/StandingsViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Tcbcsl.Presentation.Models
+{
+    public class StandingsModel
+    {
+        public int Year { get; set; }
+        public List<DivisionStandingsModel> Divisions { get; set; }
+    }
+
+    public class DivisionStandingsModel
+    {
+        public string DivisionName { get; set; }
+        public List<StandingsTeamModel> Teams { get; set; }
+    }
+
+    public class StandingsTeamModel
+    {
+        public int Year { get; set; }
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Ties { get; set; }
+        public double WinningPercentage { get; set; }
+        public double GamesBack { get; set; }
+        public int DivisionWins { get; set; }
+        public int DivisionLosses { get; set; }
+        public int DivisionTies { get; set; }
+        public int RunsScored { get; set; }
+        public int RunsAllowed { get; set; }
+        public int Streak { get; set; }
+        public int Last5Wins { get; set; }
+        public int Last5Losses { get; set; }
+        public int Last5Ties { get; set; }
+    }
+}
