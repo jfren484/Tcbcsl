639c571b5e77f9cbb7d2097382a80ae4e8592a32
jfren484 <jay@frenchfryz.com>
2015-07-06 23:12:12 -0500

    Start on Schedule controller/view

---
+++ Presentation/Controllers/ScheduleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Tcbcsl.Presentation.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        [Route("Schedule/{date:datetime?}")]
+        public ActionResult Schedule(DateTime? date)
+        {
+            var scheduleDate = date ?? DateTime.Today;
+            return View((object)scheduleDate.ToLongDateString());
+        }
+    }
+}
