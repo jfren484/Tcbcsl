61ef4e7fc810549ff5eb11f6e35eb88c73c1290e
jfren484 <jay@frenchfryz.com>
2015-07-29 17:35:39 -0400

    DataTables

--- Presentation/App_Start/BundleConfig.cs
+++ Presentation/App_Start/BundleConfig.cs
@@ -7,25 +7,26 @@ namespace Tcbcsl.Presentation
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/site.js"));
+            bundles.Add(new ScriptBundle("~/bundles/script-base").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/DataTables/jquery.dataTables.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/DataTables/dataTables.bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/site.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/DataTables/css/dataTables.bootstrap.css",
+                "~/Content/Site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
