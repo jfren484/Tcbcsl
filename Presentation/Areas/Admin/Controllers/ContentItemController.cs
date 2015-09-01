3e78b46502b29670a51147c824c2a4ba4a82cd21
jfren484 <jay@frenchfryz.com>
2015-08-31 23:51:40 -0500

    Table with page content items

--- Presentation/Areas/Admin/Controllers/ContentItemController.cs
+++ Presentation/Areas/Admin/Controllers/ContentItemController.cs
@@ -1,15 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 
 namespace Tcbcsl.Presentation.Areas.Admin.Controllers
 {
     [RouteArea("Admin")]
     [RoutePrefix("Content")]
-    public class ContentItemController : Controller
+    public class ContentItemController : Presentation.Controllers.ControllerBase
     {
         [Route("")]
         public ActionResult List()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("Data")]
+        public JsonResult Data()
+        {
+            var data = DbContext.PageContents.OrderBy(pc => pc.PageContentId);
+
+            return Json(data);
         }
     }
 }
