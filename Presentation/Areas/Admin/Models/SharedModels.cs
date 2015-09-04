f3688f9fc7c2d2278fb86ff0dc865cd38d26c097
jfren484 <jay@frenchfryz.com>
2015-09-04 09:36:51 -0400

    Missed file

---
+++ Presentation/Areas/Admin/Models/SharedModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tcbcsl.Presentation.Areas.Admin.Models
+{
+    public class AdminListTableModel
+    {
+        public string ListItemName { get; set; }
+        public string CreateItemUrl { get; set; }
+    }
+
+    public class EditModelBase
+    {
+        [Display(Name = "History")]
+        public AuditDetailsModel AuditDetails { get; set; }
+
+        [Display(Name = "")]
+        public string EditUrl { get; set; }
+    }
+
+    public class AuditDetailsModel
+    {
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? Modified { get; set; }
+    }
+}
